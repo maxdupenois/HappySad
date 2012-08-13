e6c63dbf84c63c1b515961228af78837ea4bc4c6
Max <max.dupenois@forward.co.uk>
2012-08-13 15:36:25 +0100

    Added an image switch using sinatra sessions

--- application.rb
+++ application.rb
@@ -8,14 +8,27 @@ require File.join(File.dirname(__FILE__), *%w[lib/page_scraper])
 require File.join(File.dirname(__FILE__), *%w[lib/sentiment_scorer])
 
 configure do
+  enable :sessions
   set :views, "#{File.join(File.dirname(__FILE__), *%w[views])}"
   set :public_folder, "#{File.join(File.dirname(__FILE__), *%w[assets])}"
   set :public, "#{File.join(File.dirname(__FILE__), *%w[assets])}"
 end
 
+before do
+  session[:imgtype] = "nick" if(!session.key?(:imgtype))
+  @imgtype = session[:imgtype]
+end
+
+get "/swtchimg" do
+  session[:imgtype] = (@imgtype=="fwd" ? "nick" : "fwd")
+  redirect '/'
+end
+
+
 get "/" do
   haml :index
 end
+
 
 post "/" do 
   website = params['website']
